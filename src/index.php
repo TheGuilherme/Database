03585a7d7eb7cb84d70dd17db1b5289447324bab
Guilherme Alves <36914414+TheGuilherme@users.noreply.github.com>
2018-02-28 05:52:30 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -3,24 +3,6 @@
 // Required Database.php
 require_once 'classes/Database.php';
 
-/**
- * How to use?
- * 
- * Configure the file in 'settings/database.php'
- * 
- * Instance de class Database::get()
- * 
- * Once this is done, select the required method, here we will use the select method.
- * 
- * Database::get()->select("SELECT * FROM `table_name`");
- * 
- * OR
- * 
- * Database::get()->select("SELECT * FROM `table_name` WHERE id = :id", array(
- * 	':id' => 1
- * ));
- */
-
 $select = Database::get()->select("SELECT * FROM `messages`");
 ?>
 <!DOCTYPE html>
